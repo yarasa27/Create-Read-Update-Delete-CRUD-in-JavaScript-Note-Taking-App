dff3a1e9171f75ec1edee2b74dd6ea912586c9b4
yarasa27 <elcan.abbasoff27@gmail.com>
2024-11-04 03:31:32 +0400

    light or dark theme in JS

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,3 +3,29 @@
  */
 
 'use strict';
+
+const toggleTheme = function () {
+    const /** {string} */ currentTheme = document.documentElement.getAttribute('data-theme') || 'light';
+    const /** {string} */ newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
+/**
+ * Initialize the theme
+ */
+
+const /** {string | null} */ storedTheme = localStorage.getItem('theme');
+const /** {Boolean} */ systemThemeIsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const /** {string} */ InitialTheme = storedTheme ?? (systemThemeIsDark ? 'dark' : 'light');
+document.documentElement.setAttribute('data-theme', InitialTheme);
+
+/**
+ * Attach toggleTheme to theme button click event
+ */
+
+window.addEventListener('DOMContentLoaded', function() {
+    const /** {HMTLElement} */ $themeBtn = document.querySelector('[data-theme-btn]');
+    if ($themeBtn) $themeBtn.addEventListener('click', toggleTheme);
+});
